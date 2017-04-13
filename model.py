396200c198714d7abe5948f871aacc0d82e3c13b
TakuyaShimmura <tak@c8-lab.com>
2017-04-13 09:59:23 +0900

    modify

--- model.py
+++ model.py
@@ -1,118 +1,124 @@
 # -*- coding:utf-8 -*-
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 import tensorflow as tf
 import math
-from six.moves import xrange
+
+NUM_INPUT = 6
 
 
 def loop_function(prev, num_units):
 
-    with tf.variable_scope("output"):
-        weight_init = tf.truncated_normal_initializer(stddev=1.0/math.sqrt(num_units))
-        weight = tf.get_variable('output_weight', [num_units, 6], initializer = weight_init)
+	with tf.variable_scope("output"):
+		weight_init = tf.truncated_normal_initializer(stddev=1.0/math.sqrt(num_units))
+		weight = tf.get_variable('output_weight', [num_units, NUM_INPUT], initializer = weight_init)
 
-        bias_init = tf.constant_initializer(value=0.0)
-        bias = tf.get_variable('output_bias', [6], initializer=bias_init)
+		bias_init = tf.constant_initializer(value=0.0)
+		bias = tf.get_variable('output_bias', [NUM_INPUT], initializer=bias_init)
 
-        out = tf.matmul(prev, weight) + bias
-    return out
+		out = tf.matmul(prev, weight) + bias
 
-
-
-def _prediction_decoder(decoder_inputs, initial_state, cell, num_units, feed_previous=False ,scope=None):
-
-    with tf.variable_scope(scope or "prediction") as scope:
-        state = initial_state
-        outputs = []
-        prev = None
-        for i, inp in enumerate(decoder_inputs):
-            if i > 0:
-                scope.reuse_variables()
-                if feed_previous:
-                    inp = prev
-
-            out, state = cell(inp, state)
-            pred = loop_function(out, num_units)
-            outputs.append(pred)
-            prev = pred
-    return outputs, state
+	return out
 
 
 
 
-def inference(encoder_inputs, decoder_inputs, num_units, feed_previous=False):
 
-    cell = tf.nn.rnn_cell.BasicLSTMCell(num_units)
-    cell = tf.nn.rnn_cell.MultiRNNCell(cells=[cell]*5)
-    _, state = tf.nn.rnn(cell=cell, inputs=encoder_inputs, dtype=tf.float32)
+def _prediction_decoder(decoder_inputs, initial_state, cell, num_units, feed_previous, scope=None):
 
-    outputs, state = _prediction_decoder(decoder_inputs, state,cell, num_units=num_units, feed_previous=feed_previous)
-    return outputs
+	with tf.variable_scope(scope or "prediction") as scope:
+
+		state = initial_state
+		outputs = []
+		prev = None
+		for i, inp in enumerate(decoder_inputs):
+			if i > 0:
+				scope.reuse_variables()
+				if feed_previous:
+					inp = prev
+			out, state = cell(inp, state)
+			pred = loop_function(out, num_units)
+			outputs.append(pred)
+			prev = pred
+		return outputs, state
+
+
+def inference(encoder_inputs, decoder_inputs, num_units, feed_previous=True):
+
+	encoder_cell = tf.contrib.rnn.LSTMCell(num_units=num_units, use_peepholes=True)
+	encoder_cell = tf.contrib.rnn.MultiRNNCell(cells=[encoder_cell]*3)
+
+	_, state = tf.contrib.rnn.static_rnn(encoder_cell, encoder_inputs, dtype=tf.float32)
+
+	
+
+	decoder_cell = tf.contrib.rnn.LSTMCell(num_units=num_units, use_peepholes=True)
+	decoder_cell = tf.contrib.rnn.MultiRNNCell(cells=[decoder_cell]*3)
+
+
+	outputs , _ = _prediction_decoder(decoder_inputs, state, decoder_cell, num_units=num_units, feed_previous=feed_previous)
+
+	return outputs
 
 def loss(outputs, corrects):
+	
+	with tf.name_scope("loss"): 
 
-    loss_array = []
-    for output, correct in zip(outputs, corrects):
-        loss_array.append(tf.reduce_mean(tf.abs(output-correct)))
+		loss_array = []
+		for output, correct in zip(outputs, corrects):
+			loss_array.append(tf.reduce_mean(tf.abs(output - correct)))
 
-    loss = tf.add_n(loss_array)
-    tf.summary.scalar("loss", loss)
+		loss = tf.add_n(loss_array)
+	tf.summary.scalar("loss", loss)
 
-    return loss
+	return loss
 
 def train_step(loss, learning_rate):
-    return tf.train.RMSPropOptimizer(learning_rate).minimize(loss)
+
+	with tf.name_scope("train"):
+		train = tf.train.RMSPropOptimizer(learning_rate).minimize(loss)
+	return train
 
 
-def maximum_error(outputs, corrects, stddev, mean):
+def evaluate_error(outputs, corrects, std, mean):
 
-    maximum_error = None
-    counter = 0
-    for output, correct in zip(outputs, corrects):
-        true_out = output * stddev + mean
-        true_correct = correct * stddev + mean
-        abs_error = tf.abs(true_correct - true_out)
-        max_error = tf.reduce_max(abs_error, axis=0)
-        if counter == 0:
-            maximum_error = max_error
-        else:
-            maximum_error = tf.maximum(max_error, maximum_error)
+	maximum_error = None
+	average_error = []
+	counter = 0
 
-        counter +=1
+	for output, correct in zip(outputs, corrects):
+		true_out = output * std + mean
+		true_correct = correct * std + mean
+		abs_error = tf.abs(true_correct - true_out)
+		max_error = tf.reduce_max(abs_error, axis=0)
+		mean_error = tf.reduce_mean(abs_error, axis=0)
+		average_error.append(mean_error)
 
-    tf.summary.scalar("MAX HD Error", maximum_error[0])
-    tf.summary.scalar("MAX TP Error", maximum_error[1])
-    tf.summary.scalar("MAX HM Error", maximum_error[2])
-    tf.summary.scalar("MAX SM Error", maximum_error[3])
-    tf.summary.scalar("MAX CO Error", maximum_error[4])
-    tf.summary.scalar("MAX SR Error", maximum_error[5])
+		if counter == 0:
+			maximum_error = max_error
+		else:
+			maximum_error = tf.maximum(max_error, maximum_error)
 
-    return maximum_error
+		counter +=1
+
+	average_error = tf.add_n(average_error)/counter
+
+	tf.summary.scalar("MAX HD Error", maximum_error[0])
+	tf.summary.scalar("MAX TP Error", maximum_error[1])
+	tf.summary.scalar("MAX HM Error", maximum_error[2])
+	tf.summary.scalar("MAX SM Error", maximum_error[3])
+	tf.summary.scalar("MAX CO Error", maximum_error[4])
+	tf.summary.scalar("MAX SR Error", maximum_error[5])
+
+	tf.summary.scalar("HD Error", average_error[0])
+	tf.summary.scalar("TP Error", average_error[1])
+	tf.summary.scalar("HM Error", average_error[2])
+	tf.summary.scalar("SM Error", average_error[3])
+	tf.summary.scalar("CO Error", average_error[4])
+	tf.summary.scalar("SR Error", average_error[5])
+
+	return maximum_error, average_error
 
 
-def average_error(outputs, corrects, stddev, mean):
 
-    error = []
-    counter = 0
-    for output, correct in zip(outputs, corrects):
-        true_out = output * stddev + mean
-        true_correct = correct * stddev + mean
-        abs_error = tf.abs(true_correct - true_out)
-        mean_error = tf.reduce_mean(abs_error, axis=0)
-        error.append(mean_error)
-        counter +=1
 
-    each_error = tf.add_n(error)/counter
 
-    tf.summary.scalar("HD Error", each_error[0])
-    tf.summary.scalar("TP Error", each_error[1])
-    tf.summary.scalar("HM Error", each_error[2])
-    tf.summary.scalar("SM Error", each_error[3])
-    tf.summary.scalar("CO Error", each_error[4])
-    tf.summary.scalar("SR Error", each_error[5])
-
-    return each_error
--- train.py
+++ train.py
@@ -1,21 +1,16 @@
-#-*- coding:utf-8 -*-
+# -*- coding:utf-8 -*-
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-from data_sets import DataSets
-from six.moves import xrange
+from data_set import DataSets
+import model
 import time
 import tensorflow as tf
 import random
-import model
 import numpy as np
-
 
 tf.app.flags.DEFINE_string("train_dir","data/","Work directory.")
 tf.app.flags.DEFINE_string("log_dir","data/logs/","Log directory.")
 tf.app.flags.DEFINE_string("model_dir", "data/ckpt/", "Model directory.")
+tf.app.flags.DEFINE_boolean("attention", False, "Use attention or not.")
 tf.app.flags.DEFINE_integer("num_units",128, "State size.")
 tf.app.flags.DEFINE_integer("train_step", 1000, "Step size to train.")
 tf.app.flags.DEFINE_integer("batch_size", 32, "Batch size.")
@@ -24,132 +19,137 @@ tf.app.flags.DEFINE_float("learning_rate", 0.01, "Learning rate.")
 
 
 FLAGS = tf.app.flags.FLAGS
-DATA_SETS_FILE = 'data.csv'
-TEST_SETS_FILE = 'test_data.csv'
-NUM_INPUT = 6
+DATA_SETS_FILE = 'train.csv'
+TEST_SETS_FILE = 'test.csv'
+
 TIME_STEP = 24
+NUM_INPUT = 6
 LOG_STEP = 100
 
-
 def main(_):
-    data_sets = DataSets(FLAGS.train_dir+DATA_SETS_FILE, FLAGS.train_dir+TEST_SETS_FILE)
 
-    stdev = data_sets.std
-    mean = data_sets.average
+	data_sets = DataSets(FLAGS.train_dir+DATA_SETS_FILE, FLAGS.train_dir+TEST_SETS_FILE)
 
-    print(mean)
-    print(stdev)
-    print(data_sets.max)
-    print(data_sets.min)
+	std = data_sets.std
+	mean = data_sets.average
 
-    print("create model...")
-    encoder_inputs = []
-    decoder_inputs = []
-    corrects = []
-    for i in xrange(TIME_STEP):
-        encoder_input = tf.placeholder(tf.float32, [None, NUM_INPUT])
-        decoder_input = tf.placeholder(tf.float32, [None, NUM_INPUT])
-        correct = tf.placeholder(tf.float32, [None, NUM_INPUT])
+	print("create model...")
 
-        encoder_inputs.append(encoder_input)
-        decoder_inputs.append(decoder_input)
-        corrects.append(correct)
+	encoder_inputs = []
+	decoder_inputs = []
+	corrects = []
 
-        tf.add_to_collection("encoder_input", encoder_input)
-        tf.add_to_collection("decoder_input", decoder_input)
-        tf.add_to_collection("correct", correct)
+	for i in range(TIME_STEP):
+		#各時間用のplaceholderを定義
+		encoder_input = tf.placeholder(tf.float32, [None, NUM_INPUT])
+		decoder_input = tf.placeholder(tf.float32, [None, NUM_INPUT])
+		correct = tf.placeholder(tf.float32, [None, NUM_INPUT])
+		#tf.contrib.static_rnnに渡せるようにlistに突っ込む
+		encoder_inputs.append(encoder_input)
+		decoder_inputs.append(decoder_input)
+		corrects.append(correct)
+		#実行時に各placeholderを参照するためにcollectionに突っ込む
+		tf.add_to_collection("encoder_input", encoder_input)
+		tf.add_to_collection("decoder_input", decoder_input)
+		tf.add_to_collection("correct", correct)
 
-    outputs = model.inference(encoder_inputs, decoder_inputs, FLAGS.num_units)
+	outputs = model.inference(encoder_inputs, decoder_inputs, FLAGS.num_units)
 
-    loss = model.loss(outputs, corrects)
+	loss = model.loss(outputs, corrects)
 
-    train_step = model.train_step(loss, FLAGS.learning_rate)
+	train_step = model.train_step(loss, FLAGS.learning_rate)
 
-    average_error = model.average_error(outputs, corrects, stdev, mean)
+	maximum_error, average_error = model.evaluate_error(outputs, corrects, std, mean)
 
-    maximum_error = model.maximum_error(outputs, corrects, stdev, mean)
+	init_op = tf.global_variables_initializer()
 
-    init_op = tf.global_variables_initializer()
+	summary_op = tf.summary.merge_all()
 
-    summary_op = tf.summary.merge_all()
+	with tf.Session() as sess:
 
-    with tf.Session() as sess:
+		file_writer = tf.summary.FileWriter(FLAGS.log_dir, sess.graph)
 
-    	file_writer = tf.summary.FileWriter(FLAGS.log_dir, sess.graph)
+		valid_feed = {}
+		test_feed = {}
 
-        valid_feed = {}
-        test_feed = {}
+		v_inputs, v_corrects = data_sets.validation_data
+		t_inputs, t_corrects = data_sets.test_data
 
-        v_inputs, v_corrects = data_sets.validation_data
-        t_inputs, t_corrects = data_sets.test_data
-        valid_size = v_inputs.shape[0]
-        test_size = t_inputs.shape[0]
-        v_inputs = np.transpose(v_inputs,(1,0,2))
-        t_inputs = np.transpose(t_inputs,(1,0,2))
-        v_corrects = np.transpose(v_corrects,(1,0,2))
-        t_corrects = np.transpose(t_corrects,(1,0,2))
+		valid_size = v_inputs.shape[0]
+		test_size = t_inputs.shape[0]
 
-        for i in xrange(TIME_STEP):
-            valid_feed[tf.get_collection("encoder_input")[i]] = v_inputs[i]
-            test_feed[tf.get_collection("encoder_input")[i]] = t_inputs[i]
+		v_inputs = np.transpose(v_inputs,(1,0,2))
+		t_inputs = np.transpose(t_inputs,(1,0,2))
+		v_corrects = np.transpose(v_corrects,(1,0,2))
+		t_corrects = np.transpose(t_corrects,(1,0,2))
 
+		for i in range(TIME_STEP):
+			valid_feed[tf.get_collection("encoder_input")[i]] = v_inputs[i]
+			test_feed[tf.get_collection("encoder_input")[i]] = t_inputs[i]
+			valid_feed[tf.get_collection("correct")[i]] = v_corrects[i]
+			test_feed[tf.get_collection("correct")[i]] = t_corrects[i]
 
-        for i in xrange(TIME_STEP):
-            valid_feed[tf.get_collection("correct")[i]] = v_corrects[i]
-            test_feed[tf.get_collection("correct")[i]] = t_corrects[i]
-            if i != 0:
-                valid_feed[tf.get_collection("decoder_input")[i]] = v_corrects[i-1]
-                test_feed[tf.get_collection("decoder_input")[i]] = t_corrects[i-1]
-            else:
-                valid_feed[tf.get_collection("decoder_input")[i]] = np.zeros((valid_size, NUM_INPUT))
-                test_feed[tf.get_collection("decoder_input")[i]] = np.zeros((test_size, NUM_INPUT))
+			if i != 0:
+		  		valid_feed[tf.get_collection("decoder_input")[i]] = v_corrects[i-1]
+		  		test_feed[tf.get_collection("decoder_input")[i]] = t_corrects[i-1]
+			else:
+				valid_feed[tf.get_collection("decoder_input")[i]] = np.zeros((valid_size, NUM_INPUT))
+				test_feed[tf.get_collection("decoder_input")[i]] = np.zeros((test_size, NUM_INPUT))
 
 		saver = tf.train.Saver()
-        ckpt = tf.train.get_checkpoint_state(FLAGS.model_dir)
-        if ckpt:
-        	last_model = ckpt.model_checkpoint_path
-        	print("load " + last_model)
-         	saver.restore(sess, last_model)
-        else:
-        	sess.run(init_op)
+		ckpt = tf.train.get_checkpoint_state(FLAGS.model_dir)
+		if ckpt:
+			last_model = ckpt.model_checkpoint_path
+			print("load " + last_model)
+			saver.restore(sess, last_model)
+		else:
+			print("no ckpt file. initialize variables.")
+			sess.run(init_op)
+
+		print("start training.")
+		start_time = time.time()
+		for step in range(FLAGS.train_step + 1):
+
+			train_feed = {}
+			t_inputs, t_corrects = data_sets.get_next_batch(FLAGS.batch_size)
+			t_inputs = np.transpose(t_inputs,(1,0,2))
+			t_corrects = np.transpose(t_corrects,(1,0,2))
+
+			for i in range(TIME_STEP):
+				train_feed[tf.get_collection("encoder_input")[i]] = t_inputs[i]
+				train_feed[tf.get_collection("correct")[i]] = t_corrects[i]
+				if i != 0:
+					train_feed[tf.get_collection("decoder_input")[i]] = t_corrects[i-1]
+				else:
+					train_feed[tf.get_collection("decoder_input")[i]] = np.zeros((FLAGS.batch_size, NUM_INPUT))
+
+			
+			if step % LOG_STEP == 99:
+				run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
+				run_metadata = tf.RunMetadata()
+				summary, _ = sess.run([summary_op, train_step], feed_dict=train_feed, options=run_options, run_metadata=run_metadata)
+				file_writer.add_run_metadata(run_metadata, 'step%d' % step)
+			else:
+				sess.run(train_step, feed_dict=train_feed)
+
+			if step > 0 and step % LOG_STEP == 0:
+				duration = time.time() - start_time
+				print("step%d-%d: %f sec"%(step-LOG_STEP, step, duration))
+
+				summary, loss_val, average, maximum = sess.run([summary_op,loss, average_error, maximum_error], feed_dict=valid_feed)
+
+				file_writer.add_summary(summary, step)
+				print("step%d loss=%f average error: HD=%f TP=%f HM=%f SM=%f CO=%f SR=%f"%(step, loss_val, average[0], average[1], average[2], average[3], average[4] ,average[5]))
+				print("max error: HD=%f TP=%f HM=%f SM=%f CO=%f SR=%f"%(maximum[0], maximum[1], maximum[2], maximum[3], maximum[4] , maximum[5]))
+				start_time = time.time()
+			
+
+		saver.save(sess, FLAGS.model_dir+"model.ckpt")
+		test_loss, test_average, test_maximum = sess.run([loss, average_error, maximum_error], feed_dict=test_feed)
+		print("test loss=%f average error: HD=%f TP=%f HM=%f SM=%f CO=%f SR=%f"%(test_loss, test_average[0], test_average[1], test_average[2], test_average[3], test_average[4] ,test_average[5]))
+		print("max error: HD=%f TP=%f HM=%f SM=%f CO=%f SR=%f"%(test_maximum[0], test_maximum[1], test_maximum[2], test_maximum[3], test_maximum[4] , test_maximum[5]))
 
 
-        print("start training.")
-        for step in xrange(FLAGS.train_step):
+if __name__ == '__main__':
+	tf.app.run()
 
-            start_time = time.time()
-
-            train_feed = {}
-            t_inputs, t_corrects = data_sets.get_next_batch(FLAGS.batch_size)
-            t_inputs = np.transpose(t_inputs,(1,0,2))
-            t_corrects = np.transpose(t_corrects,(1,0,2))
-
-            for i in xrange(TIME_STEP):
-                train_feed[tf.get_collection("encoder_input")[i]] = t_inputs[i]
-
-            for i in xrange(TIME_STEP):
-                train_feed[tf.get_collection("correct")[i]] = t_corrects[i]
-                if i != 0:
-                    train_feed[tf.get_collection("decoder_input")[i]] = t_corrects[i-1]
-                else:
-                    train_feed[tf.get_collection("decoder_input")[i]] = np.zeros((FLAGS.batch_size, NUM_INPUT))
-
-            sess.run(train_step, feed_dict=train_feed)
-
-            if step > 0  and step % LOG_STEP == 0:
-
-                duration = time.time() - start_time
-                print("step%d-%d: %f sec"%(step-LOG_STEP, step, duration))
-                summary, error, loss_val, m_error = sess.run([summary_op, average_error, loss, maximum_error], feed_dict=valid_feed)
-                file_writer.add_summary(summary, step)
-
-                print("step%d loss=%f average error: HD=%f TP=%f HM=%f SM=%f CO=%f SR=%f"%(step, loss_val, error[0], error[1], error[2], error[3], error[4] ,error[5]))
-                print("max error: HD=%f TP=%f HM=%f SM=%f CO=%f SR=%f"%(m_error[0], m_error[1], m_error[2], m_error[3], m_error[4] , m_error[5]))
-                
-        saver.save(sess, FLAGS.model_dir+"model.ckpt")
-        test_error, test_loss, test_m = sess.run([average_error, loss, maximum_error], feed_dict=test_feed)
-        print("test loss=%f average error: HD=%f TP=%f HM=%f SM=%f CO=%f SR=%f"%(test_loss, test_error[0], test_error[1], test_error[2], test_error[3], test_error[4] ,test_error[5]))
-        print("max error: HD=%f TP=%f HM=%f SM=%f CO=%f SR=%f"%(test_m[0], test_m[1], test_m[2], test_m[3], test_m[4] , test_m[5]))
-
-if __name__ == "__main__":
-    tf.app.run()
